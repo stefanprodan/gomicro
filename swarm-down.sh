199417a4d1f9144ee71faeb106da8f61a9516710
Stefan Prodan <stefan.prodan@gmail.com>
2016-10-18 11:45:56 +0300

    swarm setup

---
+++ swarm-down.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+set -e
+
+image="gomicro"
+
+docker service scale ${image}-proxy=0
+docker service scale ${image}-worker=0
+
+docker service rm ${image}-proxy ${image}-worker
+
+docker rmi -f $image
---
+++ swarm-up.sh
@@ -0,0 +1,33 @@
+﻿#!/bin/bash
+set -e
+
+image="gomicro"
+network="gomicro-overlay"
+
+# build image
+if [ ! "$(docker images -q  $image)" ];then
+    docker build -t $image .
+fi
+
+# create network
+if [ ! "$(docker network ls --filter name=$network -q)" ];then
+    docker network create --driver overlay $network
+fi
+
+docker service create -p 3200:3000 \
+--name "${image}-worker" \
+--network "$network" \
+-e ENV="PROD" \
+-e ROLE="worker" \
+-e PORT="3000" \
+$image 
+
+docker service create -p 3100:3000 \
+--name "${image}-proxy" \
+--network "$network" \
+-e ENV="PROD" \
+-e ROLE="proxy" \
+-e PORT="3000" \
+-e ENDPOINTS="http://${image}-worker:3000" \
+$image 
+
