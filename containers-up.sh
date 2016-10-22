e6bac9756358836f73234e9edb57f38f32e3b753
Stefan Prodan <stefan.prodan@gmail.com>
2016-10-22 19:57:26 +0300

    provide meta for consul service registry

--- containers-up.sh
+++ containers-up.sh
@@ -19,6 +19,8 @@ docker run -d -p 3020:3000 \
 --name "${image}-worker" \
 --network "$network" \
 --restart unless-stopped \
+-e "SERVICE_NAME=${image}-worker" \
+-e "SERVICE_TAGS=gomicro,production" \
 -e ENV="PROD" \
 -e ROLE="worker" \
 -e PORT="3000" \
@@ -29,6 +31,8 @@ docker run -d -p 3010:3000 \
 --name "${image}-proxy" \
 --network "$network" \
 --restart unless-stopped \
+-e "SERVICE_NAME=${image}-proxy" \
+-e "SERVICE_TAGS=gomicro,production" \
 -e ENV="PROD" \
 -e ROLE="proxy" \
 -e PORT="3000" \
@@ -40,6 +44,8 @@ docker run -d -p 3030:3000 \
 --name "${image}-monitor" \
 --network "$network" \
 --restart unless-stopped \
+-e "SERVICE_NAME=${image}-monitor" \
+-e "SERVICE_TAGS=gomicro,production" \
 -e ENV="PROD" \
 -e ROLE="monitor" \
 -e PORT="3000" \
